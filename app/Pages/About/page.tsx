2d23d338978b57acdf5fa4ee820eb0f8879e9bc1
Keochori <69148826+Keochori@users.noreply.github.com>
2024-04-17 08:44:24 +0200

    Revamped About Page

--- app/Pages/About/page.tsx
+++ app/Pages/About/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-import Portrait from '@/app/Images/portrait.png'
+import Image from "next/image";
+import Portrait from "@/app/Images/portrait.png";
+import Link from "next/link";
 
 export default function About() {
   return (
@@ -9,29 +10,37 @@ export default function About() {
       <div className=" w-[600px] h-[500px] flex">
 
         {/* Text box */}
-        <div className=" w-[300px] h-[300px]">
-            <p className="font-josefin text-slate-400 text-xl">
-                Hi! My name is Mateusz Salaga and I am a 22 year old video game
-                programmer from sweden.
-            </p>
+        <div className=" w-[450px] h-[300px]">
+          <p className="font-josefin text-slate-400 text-xl pl-3">
+            Hi! My name is Mateusz Salaga and I am a 22 year old video game
+            programmer from sweden.
+          </p>
 
-            <p className="font-josefin text-slate-400 text-xl">
-              Feel free to check out my work!
-            </p>
 
-            
+          {/* Projects Link Text */}
+          <p className="font-josefin text-slate-400 text-xl pt-8 pb-2 text-center">
+            Feel free to check out my work!
+          </p>
+          <div className="flex justify-center">
+            <Link
+              className="transition-all font-josefin text-slate-300 text-xl border-2 border-slate-300 rounded-xl p-2 hover:text-[#979797] hover:border-[#979797]"
+              href="../Pages/Projects"
+            >
+              Projects
+            </Link>
+          </div>
         </div>
 
         {/* Image box */}
         <div className="px-10 grayscale">
-            <Image
+          <Image
+            className=" rounded-xl"
             src={Portrait}
-            width={200}
-            height={200}
+            width={300}
+            height={300}
             alt="Picture of the author"
-            />
+          />
         </div>
-
       </div>
     </div>
   );
