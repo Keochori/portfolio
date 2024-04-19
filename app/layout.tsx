ba0763f4d3e4037052dd2b773427dde3a74cf469
Keochori <69148826+Keochori@users.noreply.github.com>
2024-04-19 15:18:41 +0200

    Reverting No Css Flicker Fix Attempt
    Didn't work.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <script>0</script>
         <Header/>
         {children}
       </body>
