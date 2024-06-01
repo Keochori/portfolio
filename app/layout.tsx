6549086be19fe61319eea444752f7349620077ae
Keochori <69148826+Keochori@users.noreply.github.com>
2024-06-01 17:37:10 +0200

    Tile & Description change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import Header from "./Component/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mateusz Salaga's Portfolio",
-  description: "Mateusz Salaga's Portfolio",
+  title: "Mateusz Salaga's Portfolio'",
+  description: "Mateusz Salaga - Video Game Programmer",
 };
 
 export default function RootLayout({
